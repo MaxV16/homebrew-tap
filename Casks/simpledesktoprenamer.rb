a2cf1bfa75edfda95354d813a133618098df33fa
Maksim01110 <20106742@setu.ie>
2026-09-23 23:35:08 +0100

    simpledesktoprenamer 2.0.0

--- Casks/simpledesktoprenamer.rb
+++ Casks/simpledesktoprenamer.rb
@@ -1,6 +1,6 @@
 cask "simpledesktoprenamer" do
-  version "1.0.0"
-  sha256 "82cd09e3fb0dcce8eef373c80ffc488480eafc01e0bca80630b6c444045062af"
+  version "2.0.0"
+  sha256 "0e616f24616ac4ce9549272ef8f93e7d1bacfd13c0225c094aae73103b4302a3"
 
   url "https://github.com/MaxV16/SimpleDesktopRenamer/releases/download/v#{version}/SimpleDesktopRenamer.zip"
   name "SimpleDesktopRenamer"
